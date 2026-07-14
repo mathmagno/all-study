7d0b0dd5566d25c5d3670aea4d63d8aa7782e074
math_magno <desenvolvimento@jotaja.com.br>
2026-07-14 10:59:22 -0300

    coloquei md pra ficar atento

--- go_study/main.go
+++ go_study/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go_definitive/greetings" // substitua pelo nome exato do módulo do seu go.mod
+)
+
 
 func main() {
 	fmt.Println("Hello, World!")
--- greetings/message.go
+++ greetings/message.go
@@ -1,10 +1,15 @@
 package greetings
 
-import "fmt"
-
+import (
+	"errors"
+	"fmt"
+)
 // Hello returns a greeting for the named person
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
-	message := fmt.SprintF("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty nam")
+	}
+
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
 }
