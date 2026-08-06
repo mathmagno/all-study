fae16c38607fbd4ec1b1621837d90e5e23634b10
math_magno <matheusmagno11@gmail.com>
2026-08-05 22:23:53 -0300

    comecei java!

---
+++ java_udemy/produtosapi/src/test/java/mathdev/example/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package mathdev.example.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
